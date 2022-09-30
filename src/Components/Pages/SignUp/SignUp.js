ad93a0c1fb96584735de4d596dac6dd0493434fc
Ajay Kumar <99203990+ajaykumar1071999@users.noreply.github.com>
2022-09-30 13:18:29 +0530

    br

--- src/Components/Pages/SignUp/SignUp.js
+++ src/Components/Pages/SignUp/SignUp.js
@@ -3,7 +3,7 @@ function SignUp()
 {
 const[name,setName]=useState("");
 const[email,setEmail]=useState("");
-const[Mobile,setMobile]=useState("");
+const[mobile,setMobile]=useState("");
 const[dob,setDob]=useState("");
 const[address,setAddress]=useState("");
 function GetData()
@@ -14,11 +14,11 @@ function GetData()
         <div>
             <h1>SignUp Form</h1>
 
-            <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} name="name"/>
-            <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} name="email"/>
-            <input type="text" value={mobile} onChange={(e)=>{setMobile(e.target.value)}} name="mobile"/>
-            <input type="text" value={dob} onChange={(e)=>{setDob(e.target.value)}} name="dob"/>
-            <input type="text" value={address} onChange={(e)=>{setAddress(e.target.value)}} name="address"/>
+            <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} name="name"/><br/><br/>
+            <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} name="email"/><br/><br/>
+            <input type="text" value={mobile} onChange={(e)=>{setMobile(e.target.value)}} name="mobile"/><br/><br/>
+            <input type="text" value={dob} onChange={(e)=>{setDob(e.target.value)}} name="dob"/><br/><br/>
+            <input type="text" value={address} onChange={(e)=>{setAddress(e.target.value)}} name="address"/><br/><br/>
             
             <button onClick={GetData}>GetData</button>
         </div>
