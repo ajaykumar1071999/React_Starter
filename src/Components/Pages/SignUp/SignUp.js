a37bb1ccd0791945b468f9dec7387272cf00523f
Ajay Kumar <99203990+ajaykumar1071999@users.noreply.github.com>
2022-09-30 13:13:48 +0530

    all set

--- src/Components/Pages/SignUp/SignUp.js
+++ src/Components/Pages/SignUp/SignUp.js
@@ -2,6 +2,10 @@ import {useState} from 'react';
 function SignUp()
 {
 const[name,setName]=useState("");
+const[email,setEmail]=useState("");
+const[Mobile,setMobile]=useState("");
+const[dob,setDob]=useState("");
+const[address,setAddress]=useState("");
 function GetData()
 {
     console.log(name);
@@ -11,7 +15,12 @@ function GetData()
             <h1>SignUp Form</h1>
 
             <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} name="name"/>
-            <button onClick={Getdata}>GetData</button>
+            <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} name="email"/>
+            <input type="text" value={mobile} onChange={(e)=>{setMobile(e.target.value)}} name="mobile"/>
+            <input type="text" value={dob} onChange={(e)=>{setDob(e.target.value)}} name="dob"/>
+            <input type="text" value={address} onChange={(e)=>{setAddress(e.target.value)}} name="address"/>
+            
+            <button onClick={GetData}>GetData</button>
         </div>
     );
 }export default SignUp;
