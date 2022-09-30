6fde3310ec7541dc4d1ed011cfcf7a888584ff4b
Ajay Kumar <99203990+ajaykumar1071999@users.noreply.github.com>
2022-09-30 21:57:58 +0530

    change jay

--- src/Components/Pages/SignUp/SignUp.js
+++ src/Components/Pages/SignUp/SignUp.js
@@ -1,65 +1,109 @@
-import {useState} from 'react';
-function SignUp()
-{
-const[data,setData]=useState([]);
-const[name,setName]=useState("");
-const[email,setEmail]=useState("");
-const[mobile,setMobile]=useState("");
-const[dob,setDob]=useState("");
-const[address,setAddress]=useState("");
-function GetData()
-{
-    // data = data.push({
-    //     name,
-    //     email,
-    //     mobile,
-    //     dob,
-    //     address
-    // });
+import { useState } from "react";
+var data = [];
+function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [dob, setDob] = useState("");
+  const [address, setAddress] = useState("");
+  const [res, setRes] = useState([]);
+  function GetData() {
+    let i = res.length;
+    data.push({
+      i,
+      name,
+      email,
+      mobile,
+      dob,
+      address
+    });
+    // console.log(data);
+    setRes(data);
+    setName("");
+    setEmail("");
+    setMobile("");
+    setDob("");
+    setAddress("");
+  }
+  return (
+    <div>
+      <h1>SignUp Form</h1>
 
-    setData([...data, {
-        name,
-        email,
-        mobile,
-        dob,
-        address
-    }]);
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        name="name"
+      />
+      <br />
+      <br />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        name="email"
+      />
+      <br />
+      <br />
+      <input
+        type="text"
+        value={mobile}
+        onChange={(e) => {
+          setMobile(e.target.value);
+        }}
+        name="mobile"
+      />
+      <br />
+      <br />
+      <input
+        type="text"
+        value={dob}
+        onChange={(e) => {
+          setDob(e.target.value);
+        }}
+        name="dob"
+      />
+      <br />
+      <br />
+      <input
+        type="text"
+        value={address}
+        onChange={(e) => {
+          setAddress(e.target.value);
+        }}
+        name="address"
+      />
+      <br />
+      <br />
+
+      <button onClick={GetData}>GetData</button>
+
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Email Id</th>
+            <th>Mobile No</th>
+            <th>Date of Birth</th>
+            <th>Address</th>
+          </tr>
+          {console.log(res)}
+          {res.map((item, i) => (
+            <tr key={i}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.mobile}</td>
+              <td>{item.dob}</td>
+              <td>{item.address}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
-    return(
-        <div>
-            <h1>SignUp Form</h1>
-
-            <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} name="name"/><br/><br/>
-            <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} name="email"/><br/><br/>
-            <input type="text" value={mobile} onChange={(e)=>{setMobile(e.target.value)}} name="mobile"/><br/><br/>
-            <input type="text" value={dob} onChange={(e)=>{setDob(e.target.value)}} name="dob"/><br/><br/>
-            <input type="text" value={address} onChange={(e)=>{setAddress(e.target.value)}} name="address"/><br/><br/>
-            
-            <button onClick={GetData}>GetData</button>
-
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email Id</th>
-                        <th>Mobile No</th>
-                        <th>Date of Birth</th>
-                        <th>Address</th>
-                    </tr>
-
-                    if(data.length > 0){
-                     data.map(function(index, val){
-<tr>
-                        <td>{val.name}</td>
-                        <td>{val.email}</td>
-                        <td>{val.mobile}</td>
-                        <td>{val.dob}</td>
-                        <td>{val.address}</td>
-                    </tr>
-                     })  
-                    }
-                </tbody>
-            </table>
-        </div>
-    );
-}export default SignUp;
+export default SignUp;
