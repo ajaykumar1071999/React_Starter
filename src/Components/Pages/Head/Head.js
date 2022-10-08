9781f5bb248305822c3da15217fef2589159167c
Ajay Kumar <99203990+ajaykumar1071999@users.noreply.github.com>
2022-10-08 13:47:18 +0530

    Background image

--- src/Components/Pages/Head/Head.js
+++ src/Components/Pages/Head/Head.js
@@ -1,10 +1,4 @@
-import {Link} from 'react-router-dom';
-function Head(){
-    return (
-        <div>
-            <ul>
-            <Link to="/"><li>Home</li></Link> 
-            </ul>
-        </div>
-    );
-} export default Head;
+function Head() {
+  return <div>Head</div>;
+}
+export default Head;
